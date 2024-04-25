fc05af776cab8fb81cdef31bde7bad9cb791486d
Andrew Kosenko <32103217+ageoldrain@users.noreply.github.com>
2024-04-25 15:00:53 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,6 +7,12 @@ SESSION_CONFIGS = [
     ),
 ]
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
 # in SESSION_CONFIGS, except those that explicitly override it.
 # the session config can be accessed from methods in your apps as self.session.config,
