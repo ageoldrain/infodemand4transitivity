d788a785f04cffb9874efaa3163dcecf43121554
Andrew Kosenko <32103217+ageoldrain@users.noreply.github.com>
2024-11-03 20:56:48 -0500

    Update models.py

--- coin_flip/models.py
+++ coin_flip/models.py
@@ -1,5 +1,7 @@
+# models.py
+
 from otree.api import *
-import numpy as np
+import random
 
 doc = """
 Curiosity and Information Demand with Three Coins: Fair, Biased, and Very Biased
@@ -8,50 +10,100 @@ Curiosity and Information Demand with Three Coins: Fair, Biased, and Very Biased
 class C(BaseConstants):
     NAME_IN_URL = 'economics_experiment'
     PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 10
+    NUM_BLOCKS = 10  # Number of main rounds
+    NUM_SUBROUNDS_PER_BLOCK = 3  # Each block has 3 subrounds
+    NUM_ROUNDS = NUM_BLOCKS * NUM_SUBROUNDS_PER_BLOCK  # Total rounds
 
+    COIN_PAIRS = [
+        ('fair', 'biased'),
+        ('biased', 'very biased'),
+        ('fair', 'very biased'),
+    ]
 
-class Player(BasePlayer):
-    # Player's coin choices
-    first_coin_choice = models.StringField(choices=['fair', 'biased'])
-    second_coin_choice = models.StringField(choices=['biased', 'very biased'])
-    third_coin_choice = models.StringField(choices=['fair', 'very biased'])
+    COIN_PROBABILITIES = {
+        'fair': 0.5,
+        'biased': 0.75,
+        'very biased': 0.95,
+    }
 
-    # Player's guessed outcomes for each coin
-    fair_outcome = models.StringField(choices=['H', 'T'], initial='', blank=True)
-    biased_outcome = models.StringField(choices=['H', 'T'], initial='', blank=True)
-    very_biased_outcome = models.StringField(choices=['H', 'T'], initial='', blank=True)
+class Subsession(BaseSubsession):
+    def creating_session(self):
+        for player in self.get_players():
+            # Determine block number and subround number
+            block_number = (self.round_number - 1) // C.NUM_SUBROUNDS_PER_BLOCK + 1
+            subround_number = (self.round_number - 1) % C.NUM_SUBROUNDS_PER_BLOCK
 
-    # Combined outcome choice and other fields
-    coin_permutation_choice = models.StringField(choices=['HH', 'TT', 'HT', 'TH'], initial='')
-    chosen_coin_result = models.StringField(initial='')
-    chosen_coin_permutation = models.StringField(initial='')
-    coin_permutation_result = models.StringField(initial='')
-    total_winnings = models.CurrencyField(initial=cu(0))
+            # For the first subround of each block, randomize coin pairs
+            if subround_number == 0:
+                # Randomize the order of coin pairs for this block
+                coin_pairs = C.COIN_PAIRS.copy()
+                random.shuffle(coin_pairs)
+                player.participant.vars[f'coin_pairs_block_{block_number}'] = coin_pairs
+            else:
+                # Use the existing coin_pairs for this block
+                coin_pairs = player.participant.vars[f'coin_pairs_block_{block_number}']
 
-    # New fields to store coin positions (left or right)
-    first_coin_position = models.StringField()  # Stores 'left' or 'right' for the first choice
-    second_coin_position = models.StringField()  # Stores 'left' or 'right' for the second choice
-    third_coin_position = models.StringField()  # Stores 'left' or 'right' for the third choice
-
-    def flip_chosen_coin(self, p_fair: float, p_biased: float, p_very_biased: float, chosen_coin: str):
-        assert 0 <= p_fair <= 1, "Probability for the fair coin must be between 0 and 1."
-        assert 0 <= p_biased <= 1, "Probability for the biased coin must be between 0 and 1."
-        assert 0 <= p_very_biased <= 1, "Probability for the very biased coin must be between 0 and 1."
-        
-        if chosen_coin == 'fair':
-            self.chosen_coin_result = 'H' if np.random.rand() < p_fair else 'T'
-        elif chosen_coin == 'biased':
-            self.chosen_coin_result = 'H' if np.random.rand() < p_biased else 'T'
-        elif chosen_coin == 'very biased':
-            self.chosen_coin_result = 'H' if np.random.rand() < p_very_biased else 'T'
-        
-        # Add permutation logic and winnings as needed
-        if self.coin_permutation_choice == self.coin_permutation_result:
-            self.total_winnings += cu(2)
+            # Assign the coin pair for this subround
+            player.coin1, player.coin2 = coin_pairs[subround_number]
 
 class Group(BaseGroup):
     pass
 
-class Subsession(BaseSubsession):
-    pass
+class Player(BasePlayer):
+    # Coins for the current subround
+    coin1 = models.StringField()
+    coin2 = models.StringField()
+
+    # Player's choice between the two coins
+    coin_choice = models.StringField()
+
+    # Guessed outcomes for each coin
+    coin1_outcome_guess = models.StringField(
+        choices=['H', 'T'],
+        label="Your guess for the outcome of the first coin",
+        widget=widgets.RadioSelect
+    )
+    coin2_outcome_guess = models.StringField(
+        choices=['H', 'T'],
+        label="Your guess for the outcome of the second coin",
+        widget=widgets.RadioSelect
+    )
+
+    # Actual outcomes
+    coin1_result = models.StringField()
+    coin2_result = models.StringField()
+
+    # Result of the chosen coin
+    chosen_coin_result = models.StringField()
+
+    # Total winnings (cumulative)
+    total_winnings = models.CurrencyField(initial=cu(0))
+
+    def flip_coins(self):
+        # Flip both coins and store the results
+        p_coin1 = C.COIN_PROBABILITIES[self.coin1]
+        p_coin2 = C.COIN_PROBABILITIES[self.coin2]
+        self.coin1_result = 'H' if random.random() < p_coin1 else 'T'
+        self.coin2_result = 'H' if random.random() < p_coin2 else 'T'
+
+        # Store the result of the chosen coin
+        if self.coin_choice == self.coin1:
+            self.chosen_coin_result = self.coin1_result
+        elif self.coin_choice == self.coin2:
+            self.chosen_coin_result = self.coin2_result
+
+    def calculate_winnings(self):
+        # Calculate winnings for this subround
+        round_winnings = cu(0)
+        if self.coin1_outcome_guess == self.coin1_result and self.coin2_outcome_guess == self.coin2_result:
+            round_winnings = cu(2)
+
+        # Update total winnings
+        if self.round_number == 1:
+            self.total_winnings = round_winnings
+        else:
+            previous_total = self.in_round(self.round_number - 1).total_winnings
+            self.total_winnings = previous_total + round_winnings
+
+        # Store the winnings for this round
+        self.payoff = round_winnings
